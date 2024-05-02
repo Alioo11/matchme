1adbbe49791c4894dce1dfb5c67223d7cec8b68c
Alioo11 <110mors110@gmail.com>
2024-05-02 23:30:33 +0330

    update pupeteer launch command

--- src/helpers/browser.ts
+++ src/helpers/browser.ts
@@ -1,17 +1,21 @@
 import puppeteer from "puppeteer";
 import env from "../constants/env";
 
-const linuxChromiumExecutablePath = "/usr/bin/chromium-browser --no-sandbox";
+const linuxChromiumExecutablePath = "/usr/bin/chromium-browser";
 const macOSChromiumExecutablePath =
   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
+const executablePath = env.isProd
+  ? linuxChromiumExecutablePath
+  : macOSChromiumExecutablePath;
+
+const args = env.isProd ? ["--no-sandbox", "--disable-setuid-sandbox"] : [];
+
 const createBrowser = async () => {
-  const executablePath = env.isProd
-    ? linuxChromiumExecutablePath
-    : macOSChromiumExecutablePath;
   const browser = await puppeteer.launch({
     executablePath: executablePath,
     headless: true,
+    args: args,
   });
   return browser;
 };
