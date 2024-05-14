a5c87312cf96447484b83b255fab5cc254b785fa
Alioo11 <110mors110@gmail.com>
2024-05-14 03:43:41 +0330

    return cron jobs to app.js

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,11 @@ import rankRouter from "./models/Rank/api";
 import jobAdvertRouter from "./models/JobAdvert/api";
 import connectMongoDb from "./helpers/mongo";
 import rootRouter from "./router";
-import scheduleTasks from "./tasks";
 import env from "./constants/env";
+import RankTask from "./models/Rank/task";
+import cron from 'node-cron';
+import CrawlerTask from "./models/Crawler/task";
+import CompanyTask from "./models/Company/task";
 
 const app = express();
 
@@ -22,7 +25,15 @@ app.use(cors());
 
 app.use("/api", rootRouter);
 
-scheduleTasks();
+cron.schedule("0 1 * * *", () => RankTask.updateRanking(10000)); // 1AM
+cron.schedule("30 1 * * *", () => RankTask.createRanking(100)); // 1:30AM
+cron.schedule("0 2 * * *", () => CrawlerTask.startIndexing(3000)); // 2AM
+
+cron.schedule("0 2-23 * * *", () =>
+  CompanyTask.checkCompaniesVisaSponsorShip(10)
+); // minute 0 of every hour from 2AM to 11PM
+cron.schedule("30 2-23 * * *", () => CrawlerTask.startCrawling(30)); // minute 30 of every hour from 2AM to 11PM
+
 
 app.listen(env.port, () => {
   console.log(`Server running at on port ${env.port}`);
