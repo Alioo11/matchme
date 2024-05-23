ee4126791c684a63a80ba761932e2f9d9c23a27d
Alioo11 <110mors110@gmail.com>
2024-05-23 16:25:38 +0330

    fix: removed unused files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 const cors = require("cors");
 
 import express from "express";
-import rankRouter from "./models/Rank/api";
-import jobAdvertRouter from "./models/JobAdvert/api";
 import connectMongoDb from "./helpers/mongo";
 import rootRouter from "./router";
 import env from "./constants/env";
@@ -16,21 +14,16 @@ const app = express();
 
 connectMongoDb();
 
-app.use("/jobAdvert", jobAdvertRouter); /** deprecated */
-app.use("/rank", rankRouter); /** deprecated */
-
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use("/api", rootRouter);
 
 Console.magenta("****************************************");
 Console.cyan("scheduling Tasks !");
+
 cron.schedule("0 1 * * *", () => RankTask.updateRanking(10000)); // 1AM
 cron.schedule("30 1 * * *", () => RankTask.createRanking(100)); // 1:30AM
 cron.schedule("0 2 * * *", () => CrawlerTask.startIndexing(3000)); // 2AM
-
-// cron.schedule("0 2-23 * * *", () =>CompanyTask.checkCompaniesVisaSponsorShip(10)); // minute 0 of every hour from 2AM to 11PM
 cron.schedule("30 2-23 * * *", () => CrawlerTask.startCrawling(120)); // minute 30 of every hour from 2AM to 11PM
 
 app.listen(env.port, () => {
