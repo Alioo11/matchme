019081836b005fce699d37d6de2b6730ee90574d
Alioo11 <110mors110@gmail.com>
2024-05-29 21:57:11 +0330

    update tasks

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import rootRouter from "./router";
 import env from "./constants/env";
 import RankTask from "./models/Rank/task";
 import cron from "node-cron";
-import CrawlerTask from "./models/Crawler/task";
+// import CrawlerTask from "./models/Crawler/task";
 import bodyParser from "body-parser";
 import Console from "./helpers/console";
 import CompanyTask from "./models/Company/task";
@@ -26,9 +26,9 @@ Console.cyan("scheduling Tasks !");
 
 cron.schedule("0 1 * * *", () => RankTask.updateRanking(10000)); // 1AM
 cron.schedule("30 1 * * *", () => RankTask.createRanking(100)); // 1:30AM
-cron.schedule("0 2 * * *", () => CrawlerTask.startIndexing(3000)); // 2AM
-cron.schedule("30 2-23 * * *", () => CrawlerTask.startCrawling(120)); // minute 30 of every hour from 2AM to 11PM
-cron.schedule("0 2-23 * * *", () => CompanyTask.checkCompaniesVisaSponsorShip(20)); // minute 0 of every hour from 2AM to 11PM
+// cron.schedule("0 2 * * *", () => CrawlerTask.startIndexing(3000)); // 2AM
+// cron.schedule("30 2-23 * * *", () => CrawlerTask.startCrawling(120)); // minute 30 of every hour from 2AM to 11PM
+cron.schedule("0 2-23 * * *", () => CompanyTask.checkCompaniesVisaSponsorShip(50)); // minute 0 of every hour from 2AM to 11PM
 
 app.listen(env.port, () => {
   console.log(process.env.NODE_ENV)
