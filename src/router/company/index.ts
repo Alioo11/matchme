a06c0c159335ccd8655edbdb4017013e6173d5c0
Alioo11 <110mors110@gmail.com>
2024-09-30 17:54:57 +0330

    fix: update company router

--- src/router/company/index.ts
+++ src/router/company/index.ts
@@ -6,8 +6,8 @@ const CompanyRouter = express.Router();
 CompanyRouter.get("/check-visa" , companyController.checkCompaniesVisaSponsorShip);
 CompanyRouter.get("/get-linkedin", companyController.getLinkedinURLs);
 CompanyRouter.get("/get-linkedin-users", companyController.getLinkedinCompanyUsers);
-
-
+CompanyRouter.get("/visa-status", companyController.checkVisaStatus);
+CompanyRouter.get("/", companyController.getAllCompanies);
 
 
 export default CompanyRouter;
